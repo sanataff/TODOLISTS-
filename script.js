2e2318adad1068888f0c03130c811aeb6bc09140
SANA AMMAR <sanataffor@gmail.com>
2024-07-25 16:23:12 +0300

    ...

--- script.js
+++ script.js
@@ -17,24 +17,37 @@ const checkedItems = $(".task:checked");
 const active = $(".active-filter")
 const completed = $(".completed-filter")
 const all = $(".all-filter")
-let itemNum = 2;
+let itemNum = 0;
 let tasksArray = []
-
+const clearcompleted =$(".clear-completed")
 
 // from JSON to string
 getFromLocal()
 if(localStorage.getItem("tasks")){
     tasksArray = JSON.parse(localStorage.getItem("tasks"))
+    // markAsCompleted()
     let titles = tasksArray.map(task => task.title);
-    console.log(titles)
     let ids = tasksArray.map(task => task.id);
+    let status = tasksArray.map(task => task.isCompleted)
     // ids.forEach((id)=>{
     //     title.forEach(())
     // })
-    loadingFromLocal(titles)
-
+    if(localStorage.getItem("task number")){
+        itemNum = JSON.parse(localStorage.getItem("task number"))
+ 
+        loadingFromLocal(titles, status)
+      
+        // addTaskToJson()
+    
+        removingFromUI()
+        addTaskToJson()
+        // markAsCompleted()
+        updateEventListeners()
+        
+    }
     
 }
+
 
 
 
@@ -46,50 +59,92 @@ function addTaskToArray(tasktext){
         "isCompleted": false
     }
     tasksArray.push(taskObj)
+    let itemNum = {
+        "taskNum" : 0
+    }
+    
+
+}
+function addNumtoJson(){
+    let taskNum = JSON.stringify(itemNum)
+    window.localStorage.setItem("task number",taskNum)
+   
 
 }
 function addTaskToJson(){
     let stringTask = JSON.stringify(tasksArray)
+    
     window.localStorage.setItem("tasks",stringTask)
+   
    
 }
 function getFromLocal(){
     const data = window.localStorage.getItem("tasks")
+
     if(data){
         const tasks = JSON.parse(data)
        
     }
 
+        
 }
 function updateTaskCompletion(taskId, isCompleted) {
     // Find the task object by its unique ID
-    let task = tasksArray.find(task => task.id === taskId);
+    let task = tasksArray.find(task => task.id == taskId);
     
     // If the task exists, update its isCompleted property
     if (task) {
+        
         task.isCompleted = isCompleted;
-        addTaskToJson(); // Save the updated array to local storage
+        addTaskToJson()
+
+         // Save the updated array to local storage
     }
+
+}
+function updateEventListeners() {
+    $(".task").off("click").on("click", function() {
+        let taskId = $(this).attr("id").replace("item", "");
+        let isCompleted = $(this).prop("checked");
+        let listItem = $(this).parent();
+        listItem.toggleClass("checked", isCompleted);
+        updateTaskCompletion(taskId, isCompleted);
+    });
 }
 
 // load the previouse task that are not done from local storage and add them to user interface
-function loadingFromLocal(prevTask){
-    prevTask.forEach((task) =>{
-           
-        let dynamiclist = $("<li></li>")
-        dynamiclist.addClass("list-group-item")
-        let input = $("<input>")
-        input.attr("id", `item${itemNum}`);
-        input.addClass("task")
-        input.attr("type","checkbox")
-        let label = $("<label></label>")
-        label.attr("for",`item${itemNum}`)
-        label.text(task)
-    
-        dynamiclist.append(input)
-        dynamiclist.append(label)
-        group.append(dynamiclist)
-       })
+function loadingFromLocal(prevTask, state){
+    const warapper = document.querySelectorAll(".list-group-item")
+    tasksArray.forEach((taskArr) =>{
+        if (taskArr.isCompleted == false){
+            let dynamiclist = $("<li></li>")
+                dynamiclist.addClass("list-group-item")
+                let input = $("<input>")
+                input.attr("id", `item${itemNum}`);
+                input.addClass("task")
+                input.attr("type","checkbox")
+                let label = $("<label></label>")
+                label.attr("for",`item${itemNum}`)
+                label.text(taskArr.title)
+            
+                dynamiclist.append(input)
+                dynamiclist.append(label)
+                group.append(dynamiclist)
+                itemNum ++
+                // markAsCompleted()
+                removingFromUI()
+                addTaskToJson()
+                updateEventListeners()
+                // clearCompleted()
+              
+
+                
+
+        }
+    })
+   
+
+ 
 }
 
 // adding a new task 
@@ -98,6 +153,7 @@ function addTask(){
     let task = $("input").val() 
     let dynamiclist = $("<li></li>")
     dynamiclist.addClass("list-group-item")
+    
     let input = $("<input>")
     input.attr("id", `item${itemNum}`);
     input.addClass("task")
@@ -112,7 +168,11 @@ function addTask(){
     
     addTaskToArray(task)
     markAsCompleted();
+    removingFromUI()
     addTaskToJson()
+    addNumtoJson()
+    updateEventListeners()
+    
 
 
     
@@ -124,8 +184,11 @@ function clearCompleted(){
     const toBeCleared = []
     for(let i = 0; i< taskNum();i++){
         if (warapper[i].classList.contains("checked") ){
+   
             toBeCleared.push(warapper[i])
-            // updateTaskCompletion(tasksArray.id,true)
+           
+
+            
         }
     }
     return toBeCleared
@@ -138,18 +201,22 @@ function taskNum(){
 
 }
 
-const clearcompleted =$(".clear-completed")
+
 const tasks = document.querySelectorAll(".task")
 // if the function is compleated, edit its class
 function markAsCompleted(){
     const warapper = document.querySelectorAll(".list-group-item");
     const tasks = document.querySelectorAll(".task");
+    let ids = tasksArray.map(task => task.id);
     tasks.forEach(function(task, index) {
         task.addEventListener("click", function() {
             // Add 'checked' class to the parent list item
             if (task.checked) {
                 warapper[index].classList.add("checked");
-            
+                warapper[index].setAttribute("id",ids[index])
+                console.log(warapper[index].id)
+                updateTaskCompletion(warapper[index].id,true)
+                addTaskToJson()
             } else {
                 warapper[index].classList.remove("checked");
             }
@@ -158,23 +225,31 @@ function markAsCompleted(){
 }
 
 
+
 const button = $(".btn-outline-secondary")
 button.on("click",function(){
-    
     addTask()
-    
     itemNum ++
+
 
 })
 markAsCompleted()
-
-clearcompleted.on("click",function(){
-    clearCompleted().forEach(function(taskcompleted){
-        
-        taskcompleted.remove()
+function removingFromUI(){
+    clearcompleted.on("click",function(){
+        // tasksArray.forEach((task),clearCompleted())
+        clearCompleted().forEach(function(taskcompleted){
+            // updateTaskCompletion(taskcompleted.id,true)
+            
+            taskcompleted.remove()
+    
+            itemNum --
+            
+            addNumtoJson()
+        })
+       
+    
     })
-   
 
-})
+}
+updateEventListeners()
 
-console.log(tasksArray)
